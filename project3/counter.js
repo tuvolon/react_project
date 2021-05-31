841b43ed0ec9c78be1ae0542413b4aeef80beaad
tuvolon <sachitb96@gmail.com>
2021-05-31 10:03:24 -0500

    Add files via upload

---
+++ project3/counter.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function Counter() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p> Count: {count} </p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+
+    </div>
+  );
+}
