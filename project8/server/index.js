f11d1655af13b7e59e778fef4bb35b350cb6c49a
tuvolon <sachitb96@gmail.com>
2021-06-04 13:35:15 -0500

    Update index.js

--- project8/server/index.js
+++ project8/server/index.js
@@ -19,28 +19,22 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 io.on('connection', (socket) => {
 
-  // Get the last 10 messages from the database.
   Message.find().sort({createdAt: -1}).limit(10).exec((err, messages) => {
     if (err) return console.error(err);
 
-    // Send the last messages to the user.
     socket.emit('init', messages);
   });
 
-  // Listen to connected users for a new message.
   socket.on('message', (msg) => {
-    // Create a message with the content and the name of the user.
     const message = new Message({
       content: msg.content,
       name: msg.name,
     });
 
-    // Save the message to the database.
     message.save((err) => {
       if (err) return console.error(err);
     });
 
-    // Notify all other users about a new message.
     socket.broadcast.emit('push', msg);
   });
 });
